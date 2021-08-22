caaa7cbaaa4fa5f3f0280a11b629086d6f3d048c
ellis <ems236@case.edu>
2021-08-22 10:05:37 -0400

    form input

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,14 @@
-from flask import abort, request, jsonify, make_response, send_file 
+from flask import abort, request, jsonify, make_response, send_file, render_template
 from webapp import app
 
 @app.route("/hello")
 def hello_world():
     return "Hello World"
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+@app.route("/submit")
+def submit():
+    return abort(404)
